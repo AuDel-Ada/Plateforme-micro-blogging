488dd103689dd59362f9d222af49b58494d90d50
AuDel-Ada <85498271+AuDel-Ada@users.noreply.github.com>
2021-12-07 14:38:05 +0100

    Continuité d'un projet collectif

---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use Illuminate\Support\Facades\URL;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
+
+Route::resource('articles', ArticleController::class);
+Route::resource('users', UserController::class);
+
+Route::middleware('auth')->group(function () {
+    Route::view('profile', 'auth.profile');
+    Route::name('profile')->put('profile', [RegisteredUserController::class, 'update']);
+});
