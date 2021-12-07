84898b70c816c1a9545f4bfa901d8ec152699e63
AuDel-Ada <85498271+AuDel-Ada@users.noreply.github.com>
2021-12-07 22:15:23 +0100

    Fils d'actu dans dashboard.blade
    + reprise du code original pour la photo de profil (mais qui devra être à nouveau modifié !)
    (Précédemment :
    - modif CSS Tailwind de welcome.blade)

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -16,4 +16,34 @@
         </div>
     </div>
 
+    <div class="ml-1">
+              @foreach($articles as $article)
+
+              <div class="mt-5 text-gray-600 dark:text-gray-400 text-sm">
+
+                <div>
+                  <td>{{ $article->id }}</td>
+                  <td><strong>{{ $article->created_at }}</strong></td>
+                  
+                  <div>
+                    <td>{{ $article->description }}</td>
+                    <td><img src="{{ $article->img_url }}"></td>
+                    
+                    <div  class="flex flex-row ml-20">
+                      <td><a class="button is-warning border-2 mt-2" href="">Modifier</a>
+                        <form action="" method="post">
+                          @csrf
+                          @method('DELETE')
+                          <button class="button is-danger border-2 mt-2 ml-2" type="submit">Supprimer</button>
+                        </form>
+                      </td>
+                    </div>
+                  </div>
+                </div>
+
+              </div>
+
+              @endforeach
+            </div>
+
 </x-app-layout>
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -26,9 +26,8 @@
                     <x-slot name="trigger">
                         <button class="flex items-center text-lg font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300 transition duration-150 ease-in-out">
                             
-                            <div> {{ Auth::user()->name }}
-                                <img class="w-8 mb-8 shadow-xl rounded-full mt6_" src="{{ Auth::user()->img_profil }}"> 
-                            </div>
+                        <img src="{{ Auth::user()->img_profil }}"
+                        class="w-8 mb-8 shadow-xl rounded-full mt-8"> 
                             
                             <div class="ml-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
