89f44e19bb0e3ec3232c336a33b7e32323731caf
AuDel-Ada <85498271+AuDel-Ada@users.noreply.github.com>
2021-12-16 16:59:52 +0100

    Modif suite au changement de nom du blade

---
+++ web.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Http\Controllers\ArticleController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group.
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+require __DIR__.'/auth.php';
+
+
+Route::resource('articles', ArticleController::class);
+Route::resource('users', UserController::class);
+
+// Route::resource('writeNewPost', ArticleController::create() );
+
+Route::middleware('auth')->group(function () {
+    
+    Route::view('modifProfile', 'auth.modifProfile'); //allow to go from "Profile" on navigation link, to "modify my profile" page
+    Route::name('/modifProfile')->put('modifProfile', [RegisteredUserController::class, 'update']);
+    Route::name('dashboard')->get('dashboard', [UserController::class, 'index']);
+    Route::name('dashboard')->get('dashboard', [ArticleController::class, 'show']);
+    
+});
